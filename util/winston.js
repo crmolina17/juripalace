38d13179ed7764da1b535f5d97cabbe1efc6fa0d
crmolina17 <39227602+crmolina17@users.noreply.github.com>
2019-08-16 14:54:06 -0400

    Create winston.js 16082019

---
+++ util/winston.js
@@ -0,0 +1,31 @@
+/**
+ * Winston module.
+ * @module util/winston
+ */
+
+/**
+ * Require module.
+ */
+const winston = require('winston');
+
+/**
+ * Create logger.
+ * @function createLogger
+ * @param {JSON} options - Options of config
+ */
+const log = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.File(PER.config.winston.info),
+        new winston.transports.File(PER.config.winston.error),
+        new winston.transports.File(PER.config.winston.debug)
+    ]
+});
+
+if (process.env.NODE_ENV === 'development') {
+    log.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+module.exports = log;
